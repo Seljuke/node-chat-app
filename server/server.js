088798b9dadb4e430a1c51f4128a91e176aca579
seljukes <selcukaribali@outlook.com>
2018-09-13 22:04:55 +0300

    Connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
 const express = require("express");
+const http = require("http");
 const bodyParser = require('body-parser');
 const _ = require("lodash");
 const path = require("path");
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
 
 var app = express();
-app.use(express.static(publicPath));
+var server = http.createServer(app);
+var io = socketIO(server);
 
+app.use(express.static(publicPath));
 
 app.use(bodyParser.json());
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("New User Connected");  
+    
+    socket.on("disconnect", () => {
+        console.log("User disconnected");        
+    });
+});
+
+server.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
   
