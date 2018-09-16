1b894d93f3dbbda006455c0c4725911e9a3966fa
seljukes <selcukaribali@outlook.com>
2018-09-16 22:02:31 +0300

    Welcome and user joined messages

--- public/js/index.js
+++ public/js/index.js
@@ -3,14 +3,14 @@ var socket = io();
 function sendMessage(){    
     var message = document.getElementById("m");
     var data = {
-        from: "Kullanıcı"+window.userNUMBER,
+        from: window.userNUMBER,
         text: message.value
     };
     console.log(data);
     message.value = "";
-    // var chatArea = document.getElementById("messages");
-    // chatArea.innerHTML = chatArea.innerHTML + "<li>" +
-    // data.from + ": "+ data.text + "</li>";
+    var chatArea = document.getElementById("messages");
+    chatArea.innerHTML = chatArea.innerHTML + "<li>" + new Date() +
+    " - " + window.userNUMBER + ": "+ data.text + "</li>";
 
     socket.emit("createMessage", data);
 }
@@ -27,6 +27,10 @@ socket.on("disconnect", function () {
 socket.on("newMessage", function (data) {
     console.log("New message recived: ", data);
     var chatArea = document.getElementById("messages");
-    chatArea.innerHTML = chatArea.innerHTML + "<li>" + data.createdAt + " - " + data.from + ": "+ data.text + "</li>";
+    chatArea.innerHTML = chatArea.innerHTML + "<li>" + new Date(data.createdAt) + " - " + data.from + ": "+ data.text + "</li>";
+});
+
+socket.on("newUserID", function (data) {
+    window.userNUMBER = data.id;
 });
 
--- server/server.js
+++ server/server.js
@@ -17,20 +17,43 @@ app.use(express.static(publicPath));
 app.use(bodyParser.json());
 
 io.on("connection", (socket) => {
-    console.log("New User Connected");  
+    console.log("New User Connected");
+    var userID = "Kullanıcı"+Math.floor((Math.random() * 100) + 1).toString();
+    
+    socket.emit("newUserID", {
+        id: userID
+    });
+
+    socket.emit("newMessage", {
+        from: "SERVER",
+        text: "Welcome to Chat Room " + userID,
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit("newMessage", {
+        from: "SERVER",
+        text: userID+ " joined the chat.",
+        createdAt: new Date().getTime()
+    });
 
     socket.on("createMessage", (data) =>{
         let message = _.pick(data, ["from", "text"]);
         console.log("New message came. ", message);
-        io.emit("newMessage", {
+        socket.broadcast.emit("newMessage", {
             from: message.from,
             text: message.text,
-            createdAt: new Date()
+            createdAt: new Date().getTime()
         });
     });
 
     socket.on("disconnect", () => {
-        console.log("User disconnected");        
+        console.log("User disconnected");
+        socket.broadcast.emit("newMessage", {
+            from: "SERVER",
+            text: userID+ " leave the chat room.",
+            createdAt: new Date().getTime()
+        });
+        
     });
 });
 
